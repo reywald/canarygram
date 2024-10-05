c491d8b4fbf5c60f8a0d6e87410dd833b0c579b6
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-10-05 19:01:20 +0100

    Canarygram Pictures
    Add pictures for the canaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Singer from "./assets/american_singer_canary.jpg";
+import Crest from "./assets/crested_canary.jpg";
+import Frills from "./assets/frilled_canary.jpg";
+import Harlequin from "./assets/harlequin_portuguese_canary.jpg";
+import Liz from "./assets/lizard_canary.jpg";
+import Red from "./assets/red_domestic_canary.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header className="header-component">
+        <h1>Canarygram</h1>
+      </header>
+
+      <ul className="post-list">
+        <li className="post-component">
+          <button>
+            <img src={Singer} alt="Singer" />
+            <p>Singer</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Crest} alt="Crest" />
+            <p>Crest</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Frills} alt="Frills" />
+            <p>Frills</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Harlequin} alt="Harlequin" />
+            <p>Harlequin</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Liz} alt="Liz" />
+            <p>Liz</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Red} alt="Red" />
+            <p>Red</p>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
